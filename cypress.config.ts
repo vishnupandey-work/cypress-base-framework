f648427f8bbdf91a50ceb9ebb6e28efa553ab41e
vishnupandey <vishnupandey.work@gmail.com>
2023-12-25 17:51:52 +0530

    adding cypress-mochawesome-reporter plugin

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,5 +1,4 @@
 const { defineConfig } = require("cypress");
-const { beforeRunHook, afterRunHook } = require('cypress-mochawesome-reporter/lib');
 import * as path from 'path'
 const fse = require('fs-extra')
 const locale = process.env.SITE_LOCALE;
@@ -8,17 +7,6 @@ module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
-
-      on('before:run', async (details) => {
-        console.log('override before:run');
-        await beforeRunHook(details);
-      });
-
-      on('after:run', async () => {
-        console.log('override after:run');
-        await afterRunHook();
-      });
-
       require('cypress-mochawesome-reporter/plugin')(on);
       function getConfigurationByFile(filename) {
         const pathToConfigFile = path.resolve('cypress/config', `${filename}.json`)
@@ -41,11 +29,13 @@ module.exports = defineConfig({
   trashAssestsBeforeRun: true,
   watchForFileChanges: true,
   projectId: "xmhkmk",
-  "reporterEnabled": "mochawesome, mocha-junit-reporter",
-  "mochawesomeReporterOptions": {
-    "reportDir": `.reports/${locale}`
-  },
-  "mochaJunitReporterReporterOptions": {
-    "mochaFile": `./junit/${locale}/[hash].xml`
-  },
+  reporter: 'cypress-mochawesome-reporter',
+  reporterOptions: {
+    charts: true,
+    reportPageTitle: 'custom-title',
+    embeddedScreenshots: true,
+    inlineAssets: true,
+    saveAllAttempts: false,
+    autoOpen:true
+  }
 });
