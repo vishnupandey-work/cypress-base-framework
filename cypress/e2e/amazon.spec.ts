4827e7d34058cf8806a82af94ed376ffd27f6f1c
vishnu pandey <vishnupandey.work@gmail.com>
2024-02-03 17:53:58 +0530

    adding test for amazon

---
+++ cypress/e2e/amazon.spec.ts
@@ -0,0 +1,14 @@
+describe('myTest', () => {
+
+    it('firstTest', () => {
+        cy.visit('http://www.amazon.in')
+        cy.get('#username').clear().type('dummyUser')
+        cy.get('#password').clear().type('dummyPassword')
+        cy.get('#log-in').click()
+    })
+
+    it('2nd test',()=>{
+        cy.visit('/app.html')
+        cy.get('.top-menu-controls > .element-search > input').clear().type('customerName')
+    })
+})
